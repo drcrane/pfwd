0787dadff9d15e7d51c9e08bff72560334a48999
Benjamin Green <bengreen5@yahoo.com>
2018-11-22 15:15:45 +0000

    Updated versioning strategy (now using semantic)
    https://semver.org/

--- build_win64.sh
+++ build_win64.sh
@@ -5,5 +5,5 @@
 # Order matters here, do not put -lws2_32 before the source files.
 
 rm bin/main64.exe
-x86_64-w64-mingw32-gcc -Wall -pedantic -std=c99 -o bin/main64.exe \
+x86_64-w64-mingw32-gcc -DPFWD_ENABLE_PLUGINS -DPFWD_ENABLE_HEXDUMPS -I . -I src -Wall -pedantic -std=c99 -o bin/main64.exe \
 src/main.c src/application.c -lws2_32
